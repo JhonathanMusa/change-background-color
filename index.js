9b005f3e4321de90ee8d9e17659d019830f9c94e
jhonathanmusa <jonathansm891@gmail.com>
2020-09-29 17:14:10 -0500

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const cambiarColor = () => {
   let color = document.getElementById("cambiar").value;
   document.body.style.background = color;
+  if (color == "black" || color == "#000" || color == "#333")
+    document.body.style.color = "white";
+  else {
+    document.body.style.color = "black";
+  }
 };
 
 let btnCambiar = document.getElementById("btn-cambiar");
