464ff1cd60ac7c6ea339094d8e64ba4c231525b0
jhonathanmusa <jonathansm891@gmail.com>
2020-11-11 22:03:49 -0500

    change the application to vue-js

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-const cambiarColor = () => {
+const app = new Vue({
+  el: "#app",
+  data: {
+    color: "",
+  },
+  methods: {
+    changeBackground() {
+      document.body.style.background = this.color;
+      if (
+        this.color == "black" ||
+        this.color == "#000" ||
+        this.color == "#333"
+      ) {
+        document.body.style.color = "#fff";
+      } else {
+        document.body.style.color = "#000";
+      }
+    },
+  },
+});
+
+/* const cambiarColor = () => {
   let color = document.getElementById("cambiar").value;
   document.body.style.background = color;
   if (color == "#fff" || color == "#eee" || color == "#aaa")
@@ -10,3 +31,4 @@ const cambiarColor = () => {
 
 let btnCambiar = document.getElementById("btn-cambiar");
 btnCambiar.addEventListener("click", cambiarColor);
+ */
